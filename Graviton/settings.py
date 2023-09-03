a6656d0c2ca4622912693727ea8ef4b4ff70c5ab
Shaven Wickramanayaka <ShavenDulneth@gmail.com>
2023-09-03 17:00:54 +0530

    Fixed image links and added tutorial

--- Graviton/settings.py
+++ Graviton/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-it$m)+$1-k5jxr8quc5gsc!n)%ng0(x#63ch6zj3c@tb+u$d=9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
